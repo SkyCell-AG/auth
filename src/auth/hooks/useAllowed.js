f0f0113a048db2c0a384cd068de65812590aecfb
jmiklevska <85491387+jmiklevska@users.noreply.github.com>
2021-06-16 15:44:28 +0300

    implement code-review comments

--- src/auth/hooks/useAllowed.js
+++ src/auth/hooks/useAllowed.js
@@ -3,14 +3,14 @@ import {
 } from 'react'
 import useUserRoles from './useUserRoles'
 
-const useAllowed = (allowedRoles) => {
+const useAllowed = (...requiredRoles) => {
     const roles = useUserRoles()
 
     const allowed = useMemo(() => {
-        return roles.some((r) => { return allowedRoles.some((a) => { return a === r }) })
+        return roles.some((r) => { return requiredRoles.some((a) => { return a === r }) })
     }, [
         roles,
-        allowedRoles,
+        requiredRoles,
     ])
 
     return allowed
