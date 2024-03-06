eb4c343cb69d02a542013baee57c17dc81a9c8b3
dmytrokrasnikov <dmytro.krasnikov@gmail.com>
2024-03-06 16:52:09 +0200

    update eslint-config package

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
             'error',
             {
                 peerDependencies: true,
-                devDependencies: false,
+                devDependencies: true,
             },
         ],
     },
